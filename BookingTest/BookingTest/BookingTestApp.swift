f426477f759a3c89acedd184298a8bcd36cec953
Vit Chernuhin <vit.chernuhin@icloud.com>
2023-09-05 14:54:08 +0700

    Added common packages.

---
+++ BookingTest/BookingTest/BookingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingTestApp.swift
+//  BookingTest
+//
+//  Created by Vit Chernuhin on 05.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
