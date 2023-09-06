5bf12d5da3556eb080b484fa2a4d2742f814c488
Vit Chernuhin <vit.chernuhin@icloud.com>
2023-09-06 13:57:43 +0700

    Added MessageRepresentable protocol.

---
+++ BookingTest/BookingTest/Shared/Protocols/MessageRepresentable.swift
@@ -0,0 +1,12 @@
+//
+//  MessageRepresentable.swift
+//  BookingTest
+//
+//  Created by Vit Chernuhin on 06.09.2023.
+//
+
+import Foundation
+
+protocol MessageRepresentable {
+    var message: String { get }
+}
