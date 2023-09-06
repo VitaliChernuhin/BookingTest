c75e52a94b840e0be72dfcaa939120e8a8bfddc5
Vit Chernuhin <vit.chernuhin@icloud.com>
2023-09-06 18:58:22 +0700

    Types-> added ImageViewModel.

---
+++ BookingTest/BookingTest/Shared/Types/ImageViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ImageViewModel.swift
+//  BookingTest
+//
+//  Created by Vit Chernuhin on 06.09.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct ImageViewModel: Identifiable {
+    let id = UUID()
+    let image: Image
+}
