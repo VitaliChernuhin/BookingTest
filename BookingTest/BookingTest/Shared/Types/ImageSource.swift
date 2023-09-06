9100a36bf8ffd3371b6e27d7cc06382292b48320
Vit Chernuhin <vit.chernuhin@icloud.com>
2023-09-06 20:48:41 +0700

    Types-> added ImagesSource.

---
+++ BookingTest/BookingTest/Shared/Types/ImageSource.swift
@@ -0,0 +1,15 @@
+//
+//  ImageSource.swift
+//  BookingTest
+//
+//  Created by Vit Chernuhin on 06.09.2023.
+//
+
+import Foundation
+
+struct ImageSource {
+    let downloadPath: String
+    var localPathUrl: URL?
+    
+    var isDownloaded: Bool { localPathUrl != nil }
+}
