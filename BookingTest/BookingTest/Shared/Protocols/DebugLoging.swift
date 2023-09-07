1ee76f4c4b48ff0ba99de8b43e23a1a4a18d470c
Vit Chernuhin <vit.chernuhin@icloud.com>
2023-09-07 14:12:24 +0700

    Protocols-> added DebugLoging protocol.

---
+++ BookingTest/BookingTest/Shared/Protocols/DebugLoging.swift
@@ -0,0 +1,27 @@
+//
+//  DebugLoging.swift
+//  BookingTest
+//
+//  Created by Vit Chernuhin on 07.09.2023.
+//
+
+protocol DebugLoging {
+    var source: String { get }
+    func log(message: String, method: String?)
+}
+
+extension DebugLoging {
+    var source: String {
+        String(describing: Self.self)
+    }
+    
+    func log(message: String, method: String? = nil) {
+        #if DEBUG
+        if let method = method {
+            print("\(source)->\(method)-> \(message)")
+        } else {
+            print("\(source)-> \(message)")
+        }
+        #endif
+    }
+}
